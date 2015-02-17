06e88a5a0088bd60d367cfa9b5ed338755997fcd
Martin Toledo <vizardespa@gmail.com>
2015-02-17 15:13:24 -0800

    Proyecto Base
    Ya tiene nivel 1 en el metodo "TurnoComputadora"

---
+++ IA-TicTacToe/IA-TicTacToe/Gato.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace IA_TicTacToe
+{
+    partial class Gato
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(141, 21);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(111, 92);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(258, 21);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(111, 92);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(375, 21);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(111, 92);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(141, 119);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(111, 92);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(258, 119);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(111, 92);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(375, 119);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(111, 92);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(375, 217);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(111, 92);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(258, 217);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(111, 92);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(141, 217);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(111, 92);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Turno Actual";
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Location = new System.Drawing.Point(12, 44);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(0, 13);
+            this.lblTurno.TabIndex = 10;
+            // 
+            // Gato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 374);
+            this.Controls.Add(this.lblTurno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Gato";
+            this.Text = "Gato Inteligente";
+            this.Load += new System.EventHandler(this.Gato_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTurno;
+    }
+}
+
---
+++ IA-TicTacToe/IA-TicTacToe/Gato.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IA_TicTacToe
+{
+    public partial class Gato : Form
+    {
+        public Gato()
+        {
+            InitializeComponent();
+        }
+        public string TurnoActual;
+        public int Nivel =1;
+        public string[] Botones;
+        public int[,] Matrix;
+        private void Gato_Load(object sender, EventArgs e)
+        {
+            TurnoActual = "Jugador";
+            lblTurno.Text = TurnoActual;
+            Matrix = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Matrix[i, j] = 0;
+                }
+            }
+            Botones = new string[9];
+            Botones[0] = btn1.Name;
+            Botones[1] = btn2.Name;
+            Botones[2] = btn3.Name;
+            Botones[3] = btn4.Name;
+            Botones[4] = btn5.Name;
+            Botones[5] = btn6.Name;
+            Botones[6] = btn7.Name;
+            Botones[7] = btn8.Name;
+            Botones[8] = btn9.Name;
+            btn1.Click += Click;
+            btn2.Click += Click;
+            btn3.Click += Click;
+            btn4.Click += Click;
+            btn5.Click += Click;
+            btn6.Click += Click;
+            btn7.Click += Click;
+            btn8.Click += Click;
+            btn9.Click += Click;
+        }
+
+        public void Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text == "")
+            {
+                btn.Text = TurnoActual=="Jugador"?"O":"X";
+                switch(btn.Name)
+                {
+                    case "btn1":
+                        Matrix[0,0]=TurnoActual=="Jugador"?1:2;
+                    break;
+                    case "btn2":
+                        Matrix[0, 1] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn3":
+                        Matrix[0, 2] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn4":
+                        Matrix[1, 0] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn5":
+                        Matrix[1, 1] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn6":
+                        Matrix[1, 2] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn7":
+                        Matrix[2, 0] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn8":
+                        Matrix[2, 1] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                    case "btn9":
+                        Matrix[2, 2] = TurnoActual == "Jugador" ? 1 : 2;
+                    break;
+                }
+                RevisarEstados();
+                TurnoActual =TurnoActual=="Jugador"?"Computadora":"Jugador";
+                lblTurno.Text = TurnoActual;
+                if(TurnoActual =="Computadora")
+                {
+                    TurnoComputadora();
+                }
+            }
+        }
+        public void TurnoComputadora()
+        {
+            /*btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;*/
+            if(Nivel==1)
+            {
+                Random r = new Random();
+                Button btn = this.Controls.Find(Botones[r.Next(0, 9)], true).FirstOrDefault() as Button;
+                bool v = true;
+                while(v)
+                {
+                    int l = r.Next(0, 9);
+                    btn = this.Controls.Find(Botones[l], true).FirstOrDefault() as Button;
+                    if(btn.Text=="")
+                    {
+                        v = false;
+                    }
+                }
+                btn.PerformClick();
+            }
+           /* btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;*/
+        }
+        public void RevisarEstados()
+        {
+
+            bool Ganar = false;
+            //Horizontales
+            for (int i = 0; i < 3; i++)
+            {
+                if (Matrix[0, i] == 1 && Matrix[1, i] == 1 && Matrix[2, i] == 1)
+                    Ganar = true;
+                if (Matrix[0, i] == 2 && Matrix[1, i] == 2 && Matrix[2, i] == 2)
+                    Ganar = true;
+            }
+            //Verticales
+            for (int i = 0; i < 3; i++)
+            {
+                if (Matrix[i, 0] == 1 && Matrix[i, 1] == 1 && Matrix[i, 2] == 1)
+                    Ganar = true;
+                if (Matrix[i, 0] == 2 && Matrix[i, 1] == 2 && Matrix[i, 2] == 2)
+                    Ganar = true;
+            }
+            //Diagonales
+            if (Matrix[0, 0] == 1 && Matrix[1, 1] == 1 && Matrix[2, 2] == 1)
+                Ganar = true;
+            if (Matrix[0, 0] == 2 && Matrix[1, 1] == 2 && Matrix[2, 2] == 2)
+                Ganar = true;
+            if (Matrix[0, 2] == 1 && Matrix[1, 1] == 1 && Matrix[2, 0] == 1)
+                Ganar = true;
+            if (Matrix[0, 2] == 2 && Matrix[1, 1] == 2 && Matrix[2, 0] == 2)
+                Ganar = true;
+
+            if (Ganar)
+                FinJuego(true);
+            if (ContarVacios() == 0)
+                FinJuego(false);
+        }
+        public void FinJuego(bool Estado)
+        {
+            if(Estado)
+            {
+                MessageBox.Show("Gana " + TurnoActual);
+            }
+            else
+            {
+                MessageBox.Show("Empate");
+            }
+            Reiniciar();
+        }
+        public void Reiniciar()
+        {
+            lblTurno.Text = TurnoActual;
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Matrix[i, j] = 0;
+                }
+            }
+
+        }
+        public int ContarVacios()
+        {
+            int res = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Matrix[i, j]==0)
+                        res++;
+                }
+            }
+            return res;
+        }
+
+    }
+}
